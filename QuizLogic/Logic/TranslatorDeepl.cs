9fbbe6cdbfaa49b614a649b519863163c65ff256
Kamil Podlecki <113836@student.san.edu.pl>
2023-05-23 00:46:10 +0200

    Add project files.

---
+++ QuizLogic/Logic/TranslatorDeepl.cs
@@ -0,0 +1,23 @@
+﻿using DeepL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizLogic.Logic
+{
+    internal class TranslatorDeepl
+    {
+        public string Translate(string toTranslate)
+        {
+            var authKey = "033edd9c-ca89-f3e0-cb77-4eb75c081e09:fx";
+            var translator = new Translator(authKey);
+            var translatedText = translator.TranslateTextAsync(
+            toTranslate,
+            null,
+            LanguageCode.Polish);
+            return translatedText.Result.ToString();
+        }
+    }
+}
---
+++ QuizLogic/Program.cs
@@ -0,0 +1,31 @@
+﻿using QuizLogic.Logic;
+using Trivia4NET;
+using Trivia4NET.Entities;
+using DeepL;
+using DeepL.Model;
+
+var service = new TriviaService();
+
+var response = await service.RequestTokenAsync();
+var token = response.SessionToken;
+
+var categories = await service.GetCategoriesAsync();
+var cats = categories.Categories;
+var questions = await service.GetQuestionsAsync(token,
+    amount: 1, difficulty: Difficulty.Easy, QuestionType.Multiple, 10);
+
+var question = questions.Questions[0];
+TranslatorDeepl translator = new TranslatorDeepl();
+string text = translator.Translate(question.Content);
+Console.WriteLine(text);
+
+//Console.WriteLine();
+//foreach (var category in cats)
+//{
+//    Console.WriteLine(category);
+//}
+//foreach (var item in question.IncorrectAnswers)
+//{
+//    Console.WriteLine(item);
+//}
+//Console.WriteLine(question.Answer);
